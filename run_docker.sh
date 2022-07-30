65520c5305ee4286f0b5d3f502a332fa5c2659b5
Iyke Israel <iykeprince48@gmail.com>
2022-07-30 16:40:37 +0100

    added docker

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t project-ml-microservice-kubernetes .
 
 # Step 2: 
 # List docker images
+docker images ls
 
 # Step 3: 
 # Run flask app
+docker run -d -p 5000:5000 project-ml-microservice-kubernetes
